f1cb4962e6fc663d28e3a8cb2cd078f3d01e6d30
kiran prajapati <prajapatikiran17@gmail.com>
2019-04-29 01:14:32 +0530

    updated and resolved some issues

--- dice-play.component.js
+++ dice-play.component.js
@@ -3,7 +3,10 @@ var images = [],
 indx = 0;
 let curVal = null;
 let prevVal = null;
+let holdVal = [0,0];
+let holdItem = null;
 var activeP = 1;
+var disableButton = false;
 
 images[0] = "images/dice-1.png";
 images[1] = "images/dice-2.png";
@@ -14,30 +17,52 @@ images[5] = "images/dice-6.png";
 
 
 function roleDice() {
-    console.log('we are under role dice....');
+    if (disableButton === true) {
+        document.getElementById('roleDice').disabled = true;
+    }
     img = document.getElementById('diceInstance')
     indx = (parseInt)(Math.random() * 6);
     img.src = images[indx];
     
     curVal = indx + 1;
-    if (curVal != 1) {
+    if (curVal != 1 && holdVal[activeP] <= 49) {
         var curVal = prevVal != null ? (prevVal + curVal) : curVal;
         document.getElementById("player" + activeP + "Current").innerHTML = 'Current is <br>' + curVal;
-        prevVal = curVal;
+        prevVal = curVal;      
+    } else if (holdVal[activeP] >= 50) {
+        document.getElementById('player' + activeP + 'Score').innerHTML = "Player " + activeP + " won the game!!";
+        disableButton = true;
     } else {
-        cauVal = 0;
-        prevVal = 0;
+        curVal = prevVal = holdItem = 0;
         document.getElementById("player" + activeP + "Current").innerHTML = 'Current is <br>' + curVal;
+//        document.getElementById('player' + activeP + 'Score').innerHTML = prevVal;
         activeP = activeP === 1 ? 2 : 1;
     }
 }
 
 function holdDice() {
-    document.getElementById('player' + activeP + 'Score').innerHTML = prevVal;
-    
+    if (disableButton === true) {
+        document.getElementById('roleDice').disabled = true;
+    }
+    holdItem = holdItem ? holdItem + prevVal : prevVal;
+    holdVal[activeP] = holdItem;
+    document.getElementById('player' + activeP + 'Score').innerHTML = holdItem; 
+    curVal = 0;
+    prevVal = 0;
+    document.getElementById("player" + activeP + "Current").innerHTML = 'Current is <br>' + curVal;
 }
 
 function startNewGame() {
+    disableButton = false;
+    if (disableButton === false) {
+        document.getElementById('roleDice').disabled = false;
+        document.getElementsByClassName('hold-dice')[0].disabled = false;
+    }
+    curVal = 0;
+    prevVal = 0;
+    holdItem =0;
+    holdVal = [0,0];
+    document.getElementById('player' + activeP + 'Score').innerHTML = '';
     document.getElementById("player1Current").innerHTML = 'Current is <br>' + 0;
     document.getElementById("player2Current").innerHTML = 'Current is <br>' + 0;
     document.getElementById('player1Score').innerHTML = 0;
